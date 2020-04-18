f3b2ea6b1d052a9c304a46e8e6e698267a43ef87
Zach White <zechnwhite@gmail.com>
2020-04-17 18:12:19 -0700

    adds illegal theme music lets gooo

--- main.js
+++ main.js
@@ -16,8 +16,6 @@ var config = {
   },
 };
 
-var themeMusic = new Audio('./assets/sound/theme.mp3');
-themeMusic.play();
 
 var player;
 var stars;
@@ -29,125 +27,130 @@ var scoreText;
 var game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image('sky', 'assets/sky.png');
-  this.load.image('ground', 'assets/platform.png');
-  this.load.image('star', 'assets/star.png');
-  this.load.image('bomb', 'assets/bomb.png');
-  this.load.spritesheet('dude', 'assets/dude.png', {
-    frameWidth: 32,
-    frameHeight: 48,
-  });
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 'assets/dude.png', {
+        frameWidth: 32,
+        frameHeight: 48,
+    });
 }
 
 function create() {
-  this.add.image(400, 300, 'sky');
-
-  platforms = this.physics.add.staticGroup();
-
-  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
-  platforms.create(600, 400, 'ground');
-  platforms.create(50, 250, 'ground');
-  platforms.create(750, 220, 'ground');
-
-  player = this.physics.add.sprite(100, 450, 'dude');
-
-  player.setBounce(0.2);
-  player.setCollideWorldBounds(true);
-
-  this.anims.create({
-    key: 'left',
-    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-    frameRate: 10,
-    repeat: -1,
-  });
-
-  this.anims.create({
-    key: 'turn',
-    frames: [{ key: 'dude', frame: 4 }],
-    frameRate: 20,
-  });
-
-  this.anims.create({
-    key: 'right',
-    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-    frameRate: 10,
-    repeat: -1,
-  });
-
-  cursors = this.input.keyboard.createCursorKeys();
-
-  stars = this.physics.add.group({
-    key: 'star',
-    repeat: 11,
-    setXY: { x: 12, y: 0, stepX: 70 },
-  });
-
-  stars.children.iterate(function (child) {
-    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-  });
-
-  bombs = this.physics.add.group();
-
-  scoreText = this.add.text(16, 16, 'score: 0', {
-    fontSize: '32px',
-    fill: '#000',
-  });
-
-  this.physics.add.collider(bombs, platforms);
-
-  this.physics.add.collider(player, bombs, hitBomb, null, this);
-  this.physics.add.collider(player, platforms);
-  this.physics.add.collider(stars, platforms);
-
-  this.physics.add.overlap(player, stars, collectStar, null, this);
+    this.add.image(400, 300, 'sky');
+    
+    platforms = this.physics.add.staticGroup();
+    
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
+    
+    player = this.physics.add.sprite(100, 450, 'dude');
+    
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+    
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1,
+    });
+    
+    this.anims.create({
+        key: 'turn',
+        frames: [{ key: 'dude', frame: 4 }],
+        frameRate: 20,
+    });
+    
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1,
+    });
+    
+    cursors = this.input.keyboard.createCursorKeys();
+    
+    stars = this.physics.add.group({
+        key: 'star',
+        repeat: 11,
+        setXY: { x: 12, y: 0, stepX: 70 },
+    });
+    
+    stars.children.iterate(function (child) {
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
+    
+    bombs = this.physics.add.group();
+    
+    scoreText = this.add.text(16, 16, 'score: 0', {
+        fontSize: '32px',
+        fill: '#000',
+    });
+    
+    this.physics.add.collider(bombs, platforms);
+    
+    this.physics.add.collider(player, bombs, hitBomb, null, this);
+    this.physics.add.collider(player, platforms);
+    this.physics.add.collider(stars, platforms);
+    
+    this.physics.add.overlap(player, stars, collectStar, null, this);
 }
 
 function update() {
-  if (cursors.left.isDown) {
-    player.setVelocityX(-160);
-
-    player.anims.play('left', true);
-  } else if (cursors.right.isDown) {
-    player.setVelocityX(160);
-
-    player.anims.play('right', true);
-  } else {
-    player.setVelocityX(0);
-
-    player.anims.play('turn');
-  }
-
-  if (cursors.up.isDown && player.body.touching.down) {
-    player.setVelocityY(-330);
-  }
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+        
+        player.anims.play('left', true);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+        
+        player.anims.play('right', true);
+    } else {
+        player.setVelocityX(0);
+        
+        player.anims.play('turn');
+    }
+    
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-330);
+    }
 }
 
+var themeMusic = new Audio('./assets/sound/theme.mp3');
 
 function collectStar (player, star)
+
 {
+
+    themeMusic.play();
+
     star.disableBody(true, true);
     var starSound = new Audio('./assets/sound/starCollectSoundFX.m4a');
     starSound.play();
-
+    
     score += 10;
     scoreText.setText('Score: ' + score);
-
+    
     if (stars.countActive(true) === 0)
     {
         stars.children.iterate(function (child) {
-
+            
             child.enableBody(true, child.x, 0, true, true);
-
+            
         });
-
+        
         var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-
+        
         var bomb = bombs.create(x, 16, 'bomb');
         bomb.setBounce(1);
         bomb.setCollideWorldBounds(true);
         bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-
+        
     }
 }
 
