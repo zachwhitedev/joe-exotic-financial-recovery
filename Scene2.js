18ff10dd7fccce5b07165a612fe56577cfaad4c7
Zach White <zechnwhite@gmail.com>
2020-04-18 04:14:48 -0700

    adds explosions

--- Scene2.js
+++ Scene2.js
@@ -127,7 +127,9 @@ class Scene2 extends Phaser.Scene {
     }
 
     shootBeam(){
-        var beam = new Beam(this);
+        if(this.player.active){
+            var beam = new Beam(this);
+        }
     }
 
     pickPowerUp(player, powerUp){
@@ -135,11 +137,22 @@ class Scene2 extends Phaser.Scene {
     }
 
     hurtPlayer(player, enemy){
-        this.resetShipPos(enemy);
+        if(this.player.alpha < 1){
+            return;
+        }
+        var explosion = new Explosion(this, player.x, player.y);
         player.x = config.width / 2 - 8;
         player.y = config.height - 64;
         this.score = 0;
         this.scoreLabel.text = "SCORE " + this.score;
+
+        player.disableBody(true, true);
+        this.time.addEvent({
+            delay: 1000,
+            callback: this.resetPlayer,
+            callbackScope: this,
+            loop: false
+        })
     }
     hitEnemy(projectile, enemy){
         var explosion = new Explosion(this, enemy.x, enemy.y);
@@ -149,5 +162,24 @@ class Scene2 extends Phaser.Scene {
         this.score += 10;
         this.scoreLabel.text = "SCORE " + this.score;
     }
+    resetPlayer(){
+        var x = config.width / 2 - 8;
+        var y = config.height + 64;
+        this.player.enableBody(true, x, y, true, true)
+        this.player.alpha = 0.5;
+
+        var tween = this.tweens.add({
+            targets: this.player,
+            y: config.height - 64,
+            ease: 'Power1',
+            duration: 1500,
+            repeat: 0,
+            onComplete: function(){
+                this.player.alpha = 1;
+            },
+            callbackScope: this
+        })
+    }
+
 
 }
