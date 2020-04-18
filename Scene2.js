2b5a11431cbbf5373d6dc823b332e01c4b1e1cc2
Zach White <zechnwhite@gmail.com>
2020-04-18 09:44:13 -0700

    adds files

--- Scene2.js
+++ Scene2.js
@@ -1,208 +1,187 @@
 class Scene2 extends Phaser.Scene {
-    constructor(){
-        super("playGame");
+  constructor() {
+    super('playGame');
+  }
+
+  preload() {}
+
+  create() {
+    this.background = this.add.image(0, 0, 'background');
+
+    this.cursorKeys = this.input.keyboard.createCursorKeys();
+    this.player = this.physics.add.image(20, 160, 'player');
+    this.player.setCollideWorldBounds(true);
+
+    this.agent = this.physics.add.image(300, 300, 'agent');
+    this.agent.setCollideWorldBounds(true);
+    this.agent2 = this.physics.add.image(300, 300, 'agent');
+    this.agent2.setCollideWorldBounds(true);
+
+    this.background.setOrigin(0, 0);
+    this.building = this.add.image(80, 100, 'building');
+    this.building2 = this.add.image(200, 10, 'building');
+    this.building3 = this.add.image(210, 180, 'building');
+    this.building4 = this.add.image(70, 220, 'building');
+    this.building5 = this.add.image(340, 140, 'building');
+
+    this.buildings = this.physics.add.staticGroup();
+    this.buildings.add(this.building);
+    this.buildings.add(this.building2);
+    this.buildings.add(this.building3);
+    this.buildings.add(this.building4);
+    this.buildings.add(this.building5);
+
+    this.enemies = this.add.group();
+    this.enemies.add(this.agent);
+    this.enemies.add(this.agent2);
+
+    this.agent.setVelocity(40, 40);
+    this.agent.setBounce(1);
+    this.agent2.setVelocity(70, 70);
+    this.agent2.setBounce(1);
+
+    this.meth = this.physics.add.group({
+      key: 'methbag',
+      setXY: { x: 20, y: 35 },
+      repeat: 0,
+    });
+
+    this.physics.add.collider(this.buildings, this.player);
+    this.physics.add.collider(this.enemies, this.buildings);
+    this.physics.add.collider(this.enemies, this.enemies);
+
+    this.physics.add.overlap(
+      this.player,
+      this.meth,
+      this.collectMeth,
+      null,
+      this
+    );
+    this.physics.add.overlap(
+      this.enemies,
+      this.meth,
+      this.fedsTakeMeth,
+      null,
+      this
+    );
+    this.physics.add.overlap(
+      this.player,
+      this.enemies,
+      this.gameLose,
+      null,
+      this
+    );
+
+    //////// score label ///////
+    this.graphics = this.add.graphics();
+    this.graphics.fillStyle(0x000000, 1);
+    this.graphics.beginPath();
+    this.graphics.moveTo(0, 0);
+    this.graphics.lineTo(config.width, 0);
+    this.graphics.lineTo(config.width, 20);
+    this.graphics.lineTo(0, 20);
+    this.graphics.lineTo(0, 0);
+    this.graphics.closePath();
+    this.graphics.fillPath();
+    this.laloScore = 0;
+    this.fedScore = 0;
+    this.laloScoreLabel = this.add.text(5, 5, 'Lalo: ' + this.laloScore, {
+      fontSize: '14px',
+      fill: 'lime',
+    });
+    this.fedScoreLabel = this.add.text(340, 5, 'DEA: ' + this.fedScore, {
+      fontSize: '14px',
+      fill: 'lime',
+    });
+
+    //////// sfx ///////////
+
+    this.music = this.sound.add('music');
+    var musicConfig = {
+      mute: false,
+      volume: 1,
+      rate: 1,
+      detune: 0,
+      seek: 0,
+      loop: true,
+      delay: 0,
+    };
+    this.music.play(musicConfig);
+  }
+
+  update() {
+    this.movePlayerManager();
+    if (this.laloScore >= 10) {
+      this.gameWin();
+    } else if (this.fedScore >= 10) {
+      this.gameLose();
+    }
+  }
+
+  movePlayerManager() {
+    if (this.cursorKeys.left.isDown) {
+      this.player.setVelocityX(-100);
+    } else if (this.cursorKeys.right.isDown) {
+      this.player.setVelocityX(100);
+    } else {
+      this.player.setVelocityX(0);
     }
 
-    create(){
-        this.background = this.add.tileSprite(0, 0, config.width, config.height, "background");
-        this.background.setOrigin(0, 0);
-
-        this.ship1 = this.add.sprite(config.width/2 - 50, config.height/2, "ship");
-        this.ship2 = this.add.sprite(config.width/2, config.height/2, "ship2");
-        this.ship3 = this.add.sprite(config.width/2 + 50, config.height/2, "ship3");
-
-        this.player = this.physics.add.sprite(config.width / 2 - 8, config.height - 64, "player");
-        this.player.play('thrust');
-        this.cursorKeys = this.input.keyboard.createCursorKeys();
-        this.player.setCollideWorldBounds(true);
-
-        this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-        this.projectiles = this.add.group();
-
-        this.enemies = this.physics.add.group();
-        this.enemies.add(this.ship1);
-        this.enemies.add(this.ship2);
-        this.enemies.add(this.ship3);
-
-        this.powerUps = this.physics.add.group();
-        var maxObjects = 4;
-        for(var i = 0; i <= maxObjects; i++){
-            var powerUp = this.physics.add.sprite(16, 16, "power-up");
-            this.powerUps.add(powerUp);
-            powerUp.setRandomPosition(0, 0, game.config.width, game.config.height);
-
-            if(Math.random > .5){
-                powerUp.play("red");
-            } else {
-                powerUp.play("gray");
-            }
-
-            powerUp.setVelocity(100, 100);
-            powerUp.setCollideWorldBounds(true);
-            powerUp.setBounce(1);
-        }
-
-        this.ship1.play("ship1_anim");
-        this.ship2.play("ship2_anim");
-        this.ship3.play("ship3_anim");
- 
-        this.ship1.setInteractive();
-        this.ship2.setInteractive();
-        this.ship3.setInteractive();
-
-        this.input.on('gameobjectdown', this.destroyShip, this);
-
-        this.physics.add.collider(this.projectiles, this.powerUps, function(projectile, powerUp){
-            projectile.destroy();
-        });
-        this.physics.add.overlap(this.player, this.powerUps, this.pickPowerUp, null, this);
-        this.physics.add.overlap(this.player, this.enemies, this.hurtPlayer, null, this);
-        this.physics.add.overlap(this.projectiles, this.enemies, this.hitEnemy, null, this);
-
-        //////// score label ///////
-        this.graphics = this.add.graphics();
-        this.graphics.fillStyle(0X000000, 1);
-        this.graphics.beginPath();
-        this.graphics.moveTo(0, 0);
-        this.graphics.lineTo(config.width, 0);
-        this.graphics.lineTo(config.width, 30);
-        this.graphics.lineTo(0, 30);
-        this.graphics.lineTo(0, 0);
-        this.graphics.closePath();
-        this.graphics.fillPath();
-        this.score = 0;
-        this.scoreLabel = this.add.text(8, 8, 'score: 0', {
-            fontSize: '14px',
-            fill: 'lime',
-        });
-
-        ///// sound fx
-
-        this.beamSound = this.sound.add("audio_beam");
-        this.explosionSound = this.sound.add("audio_explosion");
-        this.pickupSound = this.sound.add("audio_pickup");
-
-        this.music = this.sound.add("music");
-        var musicConfig = {
-            mute: false,
-            volume: 1,
-            rate: 1,
-            detune: 0,
-            seek: 0,
-            loop: false,
-            delay: 0
-        }
-        this.music.play(musicConfig);
+    if (this.cursorKeys.up.isDown) {
+      this.player.setVelocityY(-100);
+    } else if (this.cursorKeys.down.isDown) {
+      this.player.setVelocityY(100);
+    } else {
+      this.player.setVelocityY(0);
     }
+  }
 
-    update(){
-        this.moveShip(this.ship1, 1);
-        this.moveShip(this.ship2, .75);
-        this.moveShip(this.ship3, 1.2);
+  collectMeth(player, meth) {
+    this.laloScore += 1;
+    this.laloScoreLabel.text = 'Lalo: ' + this.laloScore;
+    meth.destroy();
+    var x =
+      player.x < 200
+        ? Phaser.Math.Between(200, 400)
+        : Phaser.Math.Between(0, 200);
+    var newMeth = this.meth.create(x, Phaser.Math.Between(40, 290), 'methbag');
+    newMeth.setBounce(1);
+    newMeth.setCollideWorldBounds(true);
+    this.physics.add.collider(this.meth, this.buildings);
+    newMeth.setVelocity(Phaser.Math.Between(-30, 30), 30);
+  }
 
-        this.background.tilePositionY -= 0.5;
-        this.movePlayerManager();
+  fedsTakeMeth(enemy, meth) {
+    this.fedScore += 1;
+    this.fedScoreLabel.text = 'DEA: ' + this.fedScore;
+    meth.destroy();
+    var x =
+      this.player.x < 200
+        ? Phaser.Math.Between(400, 800)
+        : Phaser.Math.Between(0, 400);
+    var newMeth = this.meth.create(x, 16, 'methbag');
+    newMeth.setBounce(1);
+    newMeth.setCollideWorldBounds(true);
+    this.physics.add.collider(this.meth, this.buildings);
+    newMeth.setVelocity(Phaser.Math.Between(-30, 30), 30);
+  }
 
-        if(Phaser.Input.Keyboard.JustDown(this.spacebar)){
-            this.shootBeam();
-        }
-        for(var i = 0; i < this.projectiles.getChildren().length; i++){
-            var beam = this.projectiles.getChildren()[i];
-            beam.update();
-        }
-    }
-
-    movePlayerManager(){
-        if(this.cursorKeys.left.isDown){
-            this.player.setVelocityX(-gameSettings.playerSpeed);
-        } else if (this.cursorKeys.right.isDown){
-            this.player.setVelocityX(gameSettings.playerSpeed);
-        }
-
-        if(this.cursorKeys.up.isDown){
-            this.player.setVelocityY(-gameSettings.playerSpeed);
-        } else if (this.cursorKeys.down.isDown){
-            this.player.setVelocityY(gameSettings.playerSpeed);
-        }
-    }
-
-    moveShip(ship, speed){
-        ship.y += speed;
-        if(ship.y > config.height){
-            this.resetShipPos(ship);
-        }
-    }
-
-    resetShipPos(ship){
-        ship.y = 0;
-        var randomX = Phaser.Math.Between(10, config.width - 10);
-        ship.x = randomX;
-    }
-
-    destroyShip(pointer, gameObject){
-        gameObject.setTexture("explosion");
-        gameObject.play("explode");
-    }
-
-    shootBeam(){
-        if(this.player.active){
-            this.beamSound.play();
-            var beam = new Beam(this);
-        }
-    }
-
-    pickPowerUp(player, powerUp){
-        this.pickupSound.play();
-        powerUp.disableBody(true, true);
-    }
-
-    hurtPlayer(player, enemy){
-        if(this.player.alpha < 1){
-            return;
-        }
-        var explosion = new Explosion(this, player.x, player.y);
-        player.x = config.width / 2 - 8;
-        player.y = config.height - 64;
-        this.score = 0;
-        this.scoreLabel.text = "SCORE " + this.score;
-
-        player.disableBody(true, true);
-        this.time.addEvent({
-            delay: 1000,
-            callback: this.resetPlayer,
-            callbackScope: this,
-            loop: false
-        })
-        this.explosionSound.play();
-    }
-    hitEnemy(projectile, enemy){
-        var explosion = new Explosion(this, enemy.x, enemy.y);
-
-        projectile.destroy();
-        this.resetShipPos(enemy);
-        this.score += 10;
-        this.scoreLabel.text = "SCORE " + this.score;
-
-        this.explosionSound.play();
-    }
-    resetPlayer(){
-        var x = config.width / 2 - 8;
-        var y = config.height + 64;
-        this.player.enableBody(true, x, y, true, true)
-        this.player.alpha = 0.5;
-
-        var tween = this.tweens.add({
-            targets: this.player,
-            y: config.height - 64,
-            ease: 'Power1',
-            duration: 1500,
-            repeat: 0,
-            onComplete: function(){
-                this.player.alpha = 1;
-            },
-            callbackScope: this
-        })
-    }
-
-
+  gameLose() {
+    alert('Game over!');
+    this.player.x = 38;
+    this.player.y = 10;
+    this.fedScore = 0;
+    this.fedScoreLabel.text = 'DEA: 0';
+    this.laloScore = 0;
+    this.laloScoreLabel.text = 'Lalo: 0';
+  }
+  gameWin() {
+    alert('YOU WON!!! YOU ARE A FRIEND OF THE CARTEL!! WOOHOO!!!!! $$$$$$$');
+    this.player.x = 38;
+    this.player.y = 10;
+    this.fedScore = 0;
+    this.fedScoreLabel.text = 'DEA: 0';
+    this.laloScore = 0;
+    this.laloScoreLabel.text = 'Lalo: 0';
+  }
 }
--- game.js
+++ game.js
@@ -1,20 +1,15 @@
 var config = {
-  width: 256,
-  height: 272,
-  backgroundColor: 0x000000,
+  width: 400,
+  height: 300,
   scene: [Scene1, Scene2],
   pixelArt: true,
   zoom: 2,
   physics: {
     default: "arcade",
     arcade: {
-      debug: false
+      gravity: { y: 0 }
     }
   }
 };
-
-var gameSettings = {
-  playerSpeed: 200
-}
 
 var game = new Phaser.Game(config);
