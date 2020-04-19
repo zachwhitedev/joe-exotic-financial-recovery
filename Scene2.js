2f0b8de08e4cc9d9ad8cb44726636d720dba8d65
Zach White <zechnwhite@gmail.com>
2020-04-19 00:24:33 -0700

    adds main menu

--- Scene2.js
+++ Scene2.js
@@ -237,7 +237,7 @@ class Scene2 extends Phaser.Scene {
       delay: 0,
     };
     this.neverRecover.play(recoverConfig);
-    alert('Game over! SOCRE: ' + this.totalScore);
+    alert('Game over! YOUR SCORE: ' + this.totalScore);
     this.player.x = 22;
     this.player.y = 120;
     this.laloScore = 10000;
