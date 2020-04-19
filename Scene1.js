4045be921c04c9458a5b09cc7f0a912b56c7e589
Zach White <zechnwhite@gmail.com>
2020-04-18 19:12:26 -0700

    adds files

--- Scene1.js
+++ Scene1.js
@@ -5,10 +5,16 @@ class Scene1 extends Phaser.Scene {
 
   preload() {
     this.load.image('background', 'assets/images/asphalt.png');
-    this.load.image('building', 'assets/images/building.png');
-    this.load.image('player', 'assets/images/lalo.png');
+    this.load.image('bank', 'assets/images/bank.png');
+    this.load.image('houseyellow', 'assets/images/houseyellow.png');
+    this.load.image('houseblue', 'assets/images/houseblue.png');
+    this.load.image('housepink', 'assets/images/housepink.png');
+    this.load.image('walmart', 'assets/images/walmart.png');
+    this.load.image('vertwall', 'assets/images/vertwall.png');
+    this.load.image('horwall', 'assets/images/horwall.png');
+    this.load.image('player', 'assets/images/joe.png');
+    this.load.image('tiger', 'assets/images/tiger.png');
     this.load.image('agent', 'assets/images/agent.png');
-    this.load.image('methbag', 'assets/images/methbag.png');
     this.load.audio('music', 
       'assets/sounds/theme.mp3',
     );
--- Scene2.js
+++ Scene2.js
@@ -7,67 +7,102 @@ class Scene2 extends Phaser.Scene {
 
   create() {
     this.background = this.add.image(0, 0, 'background');
-
+    this.background.setOrigin(0, 0);
+    
     this.cursorKeys = this.input.keyboard.createCursorKeys();
-    this.player = this.physics.add.image(20, 160, 'player');
+    this.player = this.physics.add.image(30, 35, 'player');
     this.player.setCollideWorldBounds(true);
 
-    this.agent = this.physics.add.image(300, 300, 'agent');
-    this.agent.setCollideWorldBounds(true);
-    this.agent2 = this.physics.add.image(300, 300, 'agent');
-    this.agent2.setCollideWorldBounds(true);
+    //////// INITIAL TIGERS ////////
+    this.tiger = this.physics.add.image(300, 100, 'tiger');
+    this.tiger.setCollideWorldBounds(true);
+    this.tiger2 = this.physics.add.image(60, 120, 'tiger');
+    this.tiger2.setCollideWorldBounds(true);
+    this.tiger3 = this.physics.add.image(40, 240, 'tiger');
+    this.tiger3.setCollideWorldBounds(true);
 
-    this.background.setOrigin(0, 0);
-    this.building = this.add.image(80, 100, 'building');
-    this.building2 = this.add.image(200, 10, 'building');
-    this.building3 = this.add.image(210, 180, 'building');
-    this.building4 = this.add.image(70, 220, 'building');
-    this.building5 = this.add.image(340, 140, 'building');
+    this.tiger.setVelocity(Phaser.Math.Between(-20, 20), 20);
+    this.tiger.setBounce(1);
+    this.tiger2.setVelocity(Phaser.Math.Between(-20, 20), -20);
+    this.tiger2.setBounce(1);
+    this.tiger3.setVelocity(Phaser.Math.Between(-20, 20), 20);
+    this.tiger3.setBounce(1);
+    //////////////////////////////////
+    
+    this.agent = this.physics.add.image(300, Phaser.Math.Between(280, 340), 'agent');
+    this.agent.setCollideWorldBounds(true);
+  
+    
+
+    ///////////// BUILDINGS /////////////////
+    this.bank = this.add.image(120, 245, 'bank');
+    this.houseyellow = this.add.image(105, 32, 'houseyellow');
+    this.houseyellow2 = this.add.image(320, 32, 'houseyellow');
+    this.houseblue = this.add.image(368, 32, 'houseblue');
+    this.houseblue2 = this.add.image(230, 32, 'houseblue');
+    this.housepink = this.add.image(185, 32, 'housepink');
+    this.walmart = this.add.image(371, 116, 'walmart');
+
+    this.vertwall = this.add.image(162, 96, 'vertwall'); //
+    this.vertwall2 = this.add.image(162, 136, 'vertwall'); //
+    this.vertwall3 = this.add.image(252, 96, 'vertwall');
+    this.vertwall4 = this.add.image(252, 134, 'vertwall');
+
+    this.horwall = this.add.image(186, 154, 'horwall');
+    this.horwall2 = this.add.image(230, 154, 'horwall');
+    this.horwall3 = this.add.image(186, 82, 'horwall');
+    this.horwall4 = this.add.image(230, 82, 'horwall');
+    
 
     this.buildings = this.physics.add.staticGroup();
-    this.buildings.add(this.building);
-    this.buildings.add(this.building2);
-    this.buildings.add(this.building3);
-    this.buildings.add(this.building4);
-    this.buildings.add(this.building5);
+    this.buildings.add(this.bank);
+    this.buildings.add(this.houseyellow);
+    this.buildings.add(this.houseyellow2);
+    this.buildings.add(this.houseblue);
+    this.buildings.add(this.houseblue2);
+    this.buildings.add(this.housepink);
+    this.buildings.add(this.walmart);
+    this.buildings.add(this.vertwall);
+    this.buildings.add(this.vertwall2);
+    this.buildings.add(this.vertwall3);
+    this.buildings.add(this.vertwall4);
+    this.buildings.add(this.horwall);
+    this.buildings.add(this.horwall2);
+    this.buildings.add(this.horwall3);
+    this.buildings.add(this.horwall4);
+    /////////////////////////////////////////
+
+
+    this.tigers = this.add.group();
+    this.tigers.add(this.tiger);
+    this.tigers.add(this.tiger2);
+    this.tigers.add(this.tiger3);
 
     this.enemies = this.add.group();
     this.enemies.add(this.agent);
-    this.enemies.add(this.agent2);
 
-    this.agent.setVelocity(40, 40);
+    this.agent.setVelocity(-10, 10);
     this.agent.setBounce(1);
-    this.agent2.setVelocity(70, 70);
-    this.agent2.setBounce(1);
 
-    this.meth = this.physics.add.group({
-      key: 'methbag',
-      setXY: { x: 20, y: 35 },
-      repeat: 0,
-    });
 
+
+    this.physics.add.collider(this.tigers, this.tigers);
+    this.physics.add.collider(this.tigers, this.buildings);
     this.physics.add.collider(this.buildings, this.player);
     this.physics.add.collider(this.enemies, this.buildings);
     this.physics.add.collider(this.enemies, this.enemies);
 
     this.physics.add.overlap(
       this.player,
-      this.meth,
-      this.collectMeth,
+      this.tigers,
+      this.collectTiger,
       null,
       this
     );
     this.physics.add.overlap(
       this.enemies,
-      this.meth,
-      this.fedsTakeMeth,
-      null,
-      this
-    );
-    this.physics.add.overlap(
-      this.player,
-      this.enemies,
-      this.gameLose,
+      this.tigers,
+      this.fedsTakeTiger,
       null,
       this
     );
@@ -83,16 +118,18 @@ class Scene2 extends Phaser.Scene {
     this.graphics.lineTo(0, 0);
     this.graphics.closePath();
     this.graphics.fillPath();
-    this.laloScore = 0;
-    this.fedScore = 0;
-    this.laloScoreLabel = this.add.text(5, 5, 'Lalo: ' + this.laloScore, {
-      fontSize: '14px',
+    this.laloScore = 10000;
+    this.laloScoreLabel = this.add.text(5, 5, 'Joe Exotic Net Worth: $' + this.laloScore, {
+      fontSize: '12px',
       fill: 'lime',
     });
-    this.fedScoreLabel = this.add.text(340, 5, 'DEA: ' + this.fedScore, {
-      fontSize: '14px',
+    this.totalScore = 0;
+    this.totalScoreLabel = this.add.text(290, 5, 'SCORE: ' + this.totalScore, {
+      fontSize: '12px',
       fill: 'lime',
     });
+
+
 
     //////// sfx ///////////
 
@@ -110,78 +147,86 @@ class Scene2 extends Phaser.Scene {
   }
 
   update() {
+    if(this.totalScore % 1000 == 0 && this.totalScore !=0){
+      this.addAgent();
+    }
+    this.totalScore += 1;
+    this.totalScoreLabel.text = 'SCORE: ' + this.totalScore;
     this.movePlayerManager();
-    if (this.laloScore >= 10) {
-      this.gameWin();
-    } else if (this.fedScore >= 10) {
+    if (this.laloScore <= -1) {
       this.gameLose();
     }
   }
 
   movePlayerManager() {
     if (this.cursorKeys.left.isDown) {
-      this.player.setVelocityX(-100);
+      this.player.setVelocityX(-120);
     } else if (this.cursorKeys.right.isDown) {
-      this.player.setVelocityX(100);
+      this.player.setVelocityX(120);
     } else {
       this.player.setVelocityX(0);
     }
 
     if (this.cursorKeys.up.isDown) {
-      this.player.setVelocityY(-100);
+      this.player.setVelocityY(-120);
     } else if (this.cursorKeys.down.isDown) {
-      this.player.setVelocityY(100);
+      this.player.setVelocityY(120);
     } else {
       this.player.setVelocityY(0);
     }
   }
 
-  collectMeth(player, meth) {
-    this.laloScore += 1;
-    this.laloScoreLabel.text = 'Lalo: ' + this.laloScore;
-    meth.destroy();
-    var x =
-      player.x < 200
-        ? Phaser.Math.Between(200, 400)
-        : Phaser.Math.Between(0, 200);
-    var newMeth = this.meth.create(x, Phaser.Math.Between(40, 290), 'methbag');
-    newMeth.setBounce(1);
-    newMeth.setCollideWorldBounds(true);
-    this.physics.add.collider(this.meth, this.buildings);
-    newMeth.setVelocity(Phaser.Math.Between(-30, 30), 30);
-  }
+  collectTiger(player, tiger) {
+    this.laloScore += 1000;
+    this.laloScoreLabel.text = 'Joe Exotic Net Worth: $' + this.laloScore;
 
-  fedsTakeMeth(enemy, meth) {
-    this.fedScore += 1;
-    this.fedScoreLabel.text = 'DEA: ' + this.fedScore;
-    meth.destroy();
-    var x =
-      this.player.x < 200
-        ? Phaser.Math.Between(400, 800)
-        : Phaser.Math.Between(0, 400);
-    var newMeth = this.meth.create(x, 16, 'methbag');
-    newMeth.setBounce(1);
-    newMeth.setCollideWorldBounds(true);
-    this.physics.add.collider(this.meth, this.buildings);
-    newMeth.setVelocity(Phaser.Math.Between(-30, 30), 30);
+    tiger.x = 200;
+    tiger.y = 128;
+    tiger.setVelocity(2);
+    var x = this.player.x < 200 ? Phaser.Math.Between(330, 380) : Phaser.Math.Between(20, 100);
+    var y = this.player.y < 150 ? Phaser.Math.Between(260, 280) : Phaser.Math.Between(20, 100);
+    var newTiger = this.physics.add.image(x, y, 'tiger');
+    newTiger.setVelocity(20);
+    newTiger.setBounce(1);
+    newTiger.setCollideWorldBounds(true);
+    this.tigers.add(newTiger);
+  }
+  
+  fedsTakeTiger(enemy, tiger) {
+    this.laloScore -= 1000;
+    this.laloScoreLabel.text = 'Joe Exotic Net Worth: $' + this.laloScore;
+    tiger.destroy();
+    var x = this.player.x < 200 ? Phaser.Math.Between(260, 380) : Phaser.Math.Between(20, 110);
+    var y = this.player.y < 150 ? Phaser.Math.Between(250, 290) : Phaser.Math.Between(10, 110);
+    var newTiger = this.physics.add.image(x, y, 'tiger');
+    newTiger.setVelocity(20);
+    newTiger.setBounce(1);
+    newTiger.setCollideWorldBounds(true);
+    this.tigers.add(newTiger);
   }
 
   gameLose() {
     alert('Game over!');
+    this.music.pause();
     this.player.x = 22;
     this.player.y = 120;
-    this.fedScore = 0;
-    this.fedScoreLabel.text = 'DEA: 0';
-    this.laloScore = 0;
-    this.laloScoreLabel.text = 'Lalo: 0';
+    this.laloScore = 10000;
+    this.laloScoreLabel.text = 'Joe Exotic Net Worth: $' + this.laloScore;
+    this.scene.start("playGame");
   }
   gameWin() {
     alert('YOU WON!!! YOU ARE A FRIEND OF THE CARTEL!! WOOHOO!!!!! $$$$$$$');
     this.player.x = 22;
     this.player.y = 120;
-    this.fedScore = 0;
-    this.fedScoreLabel.text = 'DEA: 0';
-    this.laloScore = 0;
-    this.laloScoreLabel.text = 'Lalo: 0';
+    this.laloScore = 10000;
+    this.laloScoreLabel.text = 'Joe Exotic Net Worth: $' + this.laloScore;
+  }
+  addAgent(){
+      var newAgent = this.physics.add.image(380, 350, 'agent');
+      newAgent.setVelocity(12);
+      newAgent.setBounce(1);
+      newAgent.setCollideWorldBounds(true);
+      this.enemies.add(newAgent);
+      this.added = true;
   }
 }
